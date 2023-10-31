05d4a82c767b97fc8d222ddf3335786e24649104
Tropicaljittai <93302274+Tropicaljittai@users.noreply.github.com>
2023-10-31 11:07:28 +0700

    Car movements

--- Final Project/main.py
+++ Final Project/main.py
@@ -4,7 +4,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController as fpc
 # create a window
 app = Ursina()
 
-player = fpc()
+# player = fpc()
 #models
 ground = Entity(model = 'asset/obj/ParkingLot.obj', collider = 'mesh', scale = 7, position  = (0,0,0))
 car1 = Entity(model = "asset/gltf/car_sedan.gltf", position = (-1.3,0.4,-0.6), scale = 3.5, speed = 4)
@@ -16,29 +16,35 @@ smurfcat = Entity(model = "asset/gltf/smurfs.gltf", position = (-5,0,5), scale =
 #colliders
 car1.collider = BoxCollider(car1, center=Vec3(0,0,0), size=Vec3(0.75,0.75,0.75))
 car2.collider = BoxCollider(car2, center=Vec3(0,0,0), size=Vec3(0.75,0.75,0.75))
+
 camera.position = (13,14,-40)
 camera.rotation = (18,-15,0)
 
+follow = SmoothFollow(target=car1, speed=8, offset=[0,10,-4])
+camera.add_script(follow)
+
+
 #controlling the model
 def update():
-    raycast(car1.position, direction=(1,0,0), distance=7, debug=True)
-    raycast(car1.position, direction=(-1,0,0), distance=7, debug=True)
-    raycast(car1.position, direction=(1,0,100), distance=7, debug=True)
-    raycast(car1.position, direction=(1,0,-100), distance=7, debug=True)
-    raycast(car1.position, direction=(1,0,1), distance=7, debug=True)
-    raycast(car1.position, direction=(1,0,-1), distance=7, debug=True)
-    raycast(car1.position, direction=(-1,0,-1), distance=7, debug=True)
-    raycast(car1.position, direction=(-1,0,1), distance=7, debug=True)
 
+    #Car sensors
+    r1 = raycast(car1.position, direction=(1,0,0), distance=7, debug=True)
+    r2 = raycast(car1.position, direction=(-1,0,0), distance=7, debug=True)
+    r3 = raycast(car1.position, direction=(1,0,100), distance=7, debug=True)
+    r4 = raycast(car1.position, direction=(1,0,-100), distance=7, debug=True)
+    r5 = raycast(car1.position, direction=(1,0,1), distance=7, debug=True)
+    r6 = raycast(car1.position, direction=(1,0,-1), distance=7, debug=True)
+    r7 = raycast(car1.position, direction=(-1,0,-1), distance=7, debug=True)
+    r8 = raycast(car1.position, direction=(-1,0,1), distance=7, debug=True)
+
+
+    # Car movements
     if (car1.intersects(car2).hit == False):
-        car1.rotation_y += (held_keys['right arrow'] - held_keys['left arrow'])* time.dt*150
-        car1.position += (held_keys['up arrow'] - held_keys['down arrow'])* car1.speed * time.dt * car1.forward
-    else:
-        car1.position -=  car1.speed * time.dt * car1.forward + Vec3(-1,0,0)
-    if(car2.intersects(car1).hit == False):
-        car2.rotation_y += (held_keys['j'] - held_keys['l'])* time.dt*100
-        car2.position += (held_keys['i'] - held_keys['k'])* car1.speed * time.dt * car2.forward
-    else:
-        car2.position -=  car2.speed * time.dt * car2.forward + Vec3(-1,0,0)        
+        car1.position += (held_keys['s'] - held_keys['w'])* car1.speed * time.dt * car1.forward
+        if held_keys['w']:
+            car1.rotation_y += (held_keys['d'] - held_keys['a'])* time.dt*200
+        if held_keys['s']:
+            car1.rotation_y += (held_keys['d'] - held_keys['a'])* time.dt*200
+          
 
 app.run()
